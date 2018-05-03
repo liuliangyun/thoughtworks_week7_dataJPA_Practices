93db9c95304399b4bf36eb9f04c1e646f260b6aa
unknown <you@example.com>
2018-05-03 17:39:38 +0800

    Edit entity of Company and Employee!

--- src/main/java/com/example/employee/entity/Company.java
+++ src/main/java/com/example/employee/entity/Company.java
@@ -1,5 +1,46 @@
 package com.example.employee.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Created by Liu
+ */
+@Entity
 public class Company {
 
+    @Id
+    private long id;
+    private String companyName;
+    private Integer employeesNumber;
+
+    public Company(long id, String companyName, Integer employeesNumber) {
+        this.id = id;
+        this.companyName = companyName;
+        this.employeesNumber = employeesNumber;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public Integer getEmployeesNumber() {
+        return employeesNumber;
+    }
+
+    public void setEmployeesNumber(Integer employeesNumber) {
+        this.employeesNumber = employeesNumber;
+    }
 }
--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -1,5 +1,80 @@
 package com.example.employee.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Created by Liu
+ */
+@Entity
 public class Employee {
 
+    @Id
+    private long id;
+    private String name;
+    private Integer age;
+    private String gender;
+    private long companyId;
+    private Integer salary;
+
+    public Employee() {
+
+    }
+
+    public Employee(String name, Integer age, String gender, Integer salary, long id, long companyId) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+        this.id = id;
+        this.companyId = companyId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public long getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(long companyId) {
+        this.companyId = companyId;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
 }
